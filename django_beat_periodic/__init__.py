5cde2353eefb134985250ba5a261972c62452c27
Waseem Kntar <waseem.kntar@gmail.com>
2026-02-11 16:57:02 +0300

    Release v0.4.0

--- django_beat_periodic/__init__.py
+++ django_beat_periodic/__init__.py
@@ -1,6 +1,6 @@
 """django-beat-periodic — auto-sync @periodic_task decorators to django-celery-beat."""
 
-__version__ = "0.3.0"
+__version__ = "0.4.0"
 
 from django_beat_periodic.decorators import periodic_task
 from django_beat_periodic.sync import sync_periodic_tasks
